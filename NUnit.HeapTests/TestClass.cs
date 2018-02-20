2a7e66a5250ef633c5c338d0052b736f2f3c43ca
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2018-02-20 22:36:50 +0100

    A first, quite rudimentary, test is in place. It passes :)

---
+++ NUnit.HeapTests/TestClass.cs
@@ -0,0 +1,25 @@
+﻿using Heap;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnit.HeapTests
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void DefaultConstructor_CreatesEmptyHeap()
+        {
+            var heap = new Heap<int>();
+
+            Assert.AreEqual(true, heap.Empty);
+            Assert.AreEqual(0, heap.Size);
+            Assert.Throws<InvalidOperationException>(() => heap.Peek());
+            Assert.Throws<InvalidOperationException>(() => heap.Pop());
+        }
+    }
+}
